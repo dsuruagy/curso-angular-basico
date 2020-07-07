e6b9788bcd2edecb0bf2e8a836ebb49eb518fcda
Daniel Suruagy <dsuruagy@gmail.com>
2020-07-07 11:55:19 -0300

    v0.6.0

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Hulk'},
+  {id: 2, name: 'Homem-Aranha'},
+  {id: 3, name: 'Homem de Ferro'},
+  {id: 4, name: 'Viúva Negra'},
+  {id: 5, name: 'Pantera Negra'},
+  {id: 6, name: 'Superman'},
+  {id: 7, name: 'Batman'},
+  {id: 8, name: 'Wonder Woman'}
+];
